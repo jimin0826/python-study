10d7c5ed643339d61fb8e8becb26db66391df173
JIMIN <hanjimin0826@gmail.com>
2020-09-23 23:29:32 +0900

    0923

--- python-study/numerical_mathematics/differentiation/second_order_df.py
+++ python-study/numerical_mathematics/differentiation/second_order_df.py
@@ -19,7 +19,7 @@ def BDF3(x):
   result += (11*f(x) - 18*f(x - h) + 9*f(x- 2*h) - 2*f(x - 3*h))
   return result / (6*h)
 
-print("Single Step Derivative at x = {} >> {}, error = {}".format(x, single_step_DF(x), (12-single_step_DF(x))/12*100))
+print("Single Step Derivative at x = {} >> {}, error = {}".format(x, single_step_DF(x), (12-single_step_DF(x))/12*100)) 
 print("BDF2 Derivative at x = {} >> {}, error = {}".format(x, BDF2(x), (12-BDF2(x))/12*100))
 print("BDF3 Derivative at x = {} >> {}, error = {}".format(x, BDF3(x), (12-BDF3(x))/12*100))
 
